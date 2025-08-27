d6177a31d0f51cd02fd6709e677b08d0239bdef5
louissssMultiDevice <theboyscuyy@gmail.com>
2025-08-27 22:05:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -37,4 +37,4 @@ app.post("/chat", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server berjalan di http://localhost:3000"));
+app.listen(3000, () => console.log("Server berjalan di http://info-dunia.hanamicloud.biz.id"));
